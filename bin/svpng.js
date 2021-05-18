28554fa32d57df13ec330e3a4df152172b6080bb
Jake Hickenlooper <jake@weboftomorrow.com>
2021-05-18 06:08:34 -0600

    Update to latest node and drop stuff
    Very selfish at this point since the piecemaker python project already
    has the image size information and the dependencies on svgo, image-size,
    and others were outdated.
    The tests and such were removed since this fork isn't directly using
    them at this point.  Again, this is probably something that will be
    added back in later when time permits.

--- bin/svpng.js
+++ bin/svpng.js
@@ -10,27 +10,18 @@ program
 	.description(pkg.description)
 	.arguments('<source> <output>')
 	.helpOption('-H, --help', 'output usage information')
-	.option('-h, --height <number>', 'set the height of the output image')
-	.option('-w, --width <number>', 'set the width of the output image')
-	.option('-p, --padding <number>', 'set the amount of padding around output image', 0)
+	.requiredOption('-h, --height <number>', 'set the height of the output image')
+	.requiredOption('-w, --width <number>', 'set the width of the output image')
 	.option('-b, --backgroundColor <color>', 'set the background color of the output image as any valid CSS color')
 	.option('-f, --defaultSvgLength <number>', 'width and height to render output if SVG dimensions are invalid', convert.DEFAULTS.defaultSvgLength)
 	.option('-y, --overwrite', 'overwrite output file if exists', false)
 	.option('-t, --trim', 'trim the output image to the bounds of the SVG', false)
 	.option('-o, --opaque', 'save the output image with an opaque background', false)
-	.action(async (source, dest) => {
+	.action(async (source, dest, options) => {
 		const hrstart = process.hrtime();
 
-		const options = {
-			defaultSvgLength: program.defaultSvgLength,
-			backgroundColor: program.backgroundColor,
-			omitBackground: !program.opaque,
-			overwrite: program.overwrite,
-			padding: program.padding,
-			height: program.height,
-			width: program.width,
-			trim: program.trim
-		};
+		options.width = parseInt(options.width);
+		options.height = parseInt(options.height);
 
 		try {
 			await convert(source, dest, options);
--- index.js
+++ index.js
@@ -1,54 +1,10 @@
-/* eslint no-unused-vars: ["error", { "caughtErrors": "none" }] */
-'use strict';
+const path = require("path");
+const fs = require("fs");
+const ejs = require("ejs");
+const puppeteer = require("puppeteer");
 
-const path = require('path');
-const fs = require('fs');
-const ejs = require('ejs');
-const Svgo = require('svgo');
-const size = require('image-size');
-const puppeteer = require('puppeteer');
-const opts = require('./svgo-options');
-
-const template = path.join(__dirname, 'template', 'index.html.ejs');
+const template = path.join(__dirname, "template", "index.html.ejs");
 const render = ejs.compile(`${fs.readFileSync(template)}`);
-const svgo = new Svgo(opts);
-
-/**
- * Sets SVG dimensions based on user render options and returns rendered SVG dimensions
- * to set viewport to match before taking screnshot.
- *
- * @param {Number} width - Desired SVG width
- * @param {Number} height - Desired SVG height
- * @param {Boolean} trim - Whether or not to trim the SVG to its bounds
- * @return {Object} - Rendered SVG dimensions
- */
-/* istanbul ignore next */
-const getComputedDims = (width, height, trim) => {
-	const svg = document.querySelector('svg');
-
-	if (trim) {
-		// If trimming SVG to immediate bounds, retrieve
-		// dimensions based on bounding box, not the element
-		const bbox = svg.getBBox();
-		const viewBox = [bbox.x, bbox.y, bbox.width, bbox.height].join(' ');
-		svg.setAttribute('viewBox', viewBox);
-	}
-
-	// Remove height and width attributes and set height and width
-	// to desired values; SVG will scale itself proportionally
-	svg.removeAttribute('width');
-	svg.removeAttribute('height');
-	svg.style.height = `${height}px`;
-	svg.style.width = `${width}px`;
-
-	// Get the computed dimensions, maintaining aspect ratio
-	const computed = window.getComputedStyle(svg);
-
-	return {
-		w: parseInt(computed.getPropertyValue('width'), 10),
-		h: parseInt(computed.getPropertyValue('height'), 10)
-	};
-};
 
 /**
  * Creates a PNG from an SVG with rendering based on passed options.
@@ -62,7 +18,7 @@ const convertSvgToPng = async (src, dest, options) => {
 	// Merge passed options with defaults
 	options = {
 		...convertSvgToPng.DEFAULTS,
-		...options
+		...options,
 	};
 
 	// Force omitBackground to false if backgroundColor is set
@@ -80,38 +36,25 @@ const convertSvgToPng = async (src, dest, options) => {
 		throw new Error(`File exists at "${dest}".`);
 	}
 
-	// If width or height options are set, we should ignore the SVG size attributes
-	const ignoreSizeAttributes = (options.width || options.height);
-
 	// Use fallback svg length in case size cannot be determined
 	let info = {
 		width: options.defaultSvgLength,
-		height: options.defaultSvgLength
+		height: options.defaultSvgLength,
 	};
 
-	try {
-		// Attempt to get the SVG's dimensions
-		info = size(src);
-	} catch (error) {
-		// If size cannot be determined, force trim option
-		options.trim = true;
-	}
-
 	// Retrieve contents of SVG file
-	const svg = `${fs.readFileSync(src, 'utf8')}`;
-
-	// Optimize SVG content
-	const {data} = await svgo.optimize(svg, {path: src});
+	const svg = `${fs.readFileSync(src, "utf8")}`;
 
 	// Create an HTML page with the SVG embedded
 	const markup = render({
-		svg: data,
-		padding: options.padding,
-		backgroundColor: options.backgroundColor
+		svg: svg,
+		backgroundColor: options.backgroundColor,
 	});
 
 	// Launch a puppeteer instance
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch(
+		{defaultViewport: {width: 1920, height: 1080}}
+	);
 
 	// Create a puppeteer browser
 	const page = await browser.newPage();
@@ -120,40 +63,22 @@ const convertSvgToPng = async (src, dest, options) => {
 	await page.setContent(markup);
 
 	// Set width and height based on passed options
-	let {width, height} = options;
+	let { width, height } = options;
 
-	// If we should not ignore SVG size attributes,
-	// set width and height to option value, or
-	// measured value, or default SVG length value
-	if (!ignoreSizeAttributes) {
-		width = width || info.width;
-		height = height || info.height;
-	}
-
-	// If the image is padded, reduce size of SVG
-	if (options.padding > 0) {
-		width -= (options.padding * 2);
-		height -= (options.padding * 2);
-	}
-
-	// Retrieve rendered SVG dimensions according to
-	// browser after optionally trimming SVG, and setting
-	// desired dimensions
-	const computedDims = await page.evaluate(getComputedDims, width, height, options.trim);
 
 	// Set the size of the browser viewport to match
 	// the computed dimensions of the SVG
 	await page.setViewport({
-		width: computedDims.w + (options.padding * 2),
-		height: computedDims.h + (options.padding * 2),
-		deviceScaleFactor: 1
+		width: width,
+		height: height,
+		deviceScaleFactor: 1,
 	});
 
 	// Take a screenshot of the browser, saving output to
 	// destination location, with optional transparent background
 	await page.screenshot({
 		omitBackground: options.omitBackground,
-		path: dest
+		path: dest,
 	});
 
 	// Close browser instance
@@ -171,7 +96,6 @@ convertSvgToPng.DEFAULTS = {
 	height: null,
 	width: null,
 	trim: false,
-	padding: 0
 };
 
 module.exports = convertSvgToPng;
